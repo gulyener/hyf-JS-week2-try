d9c1ac675fbe38f471d01fd67fec99c616cc6e28
Gul <gulpembeyener@gmail.com>
2019-06-16 15:14:54 +0200

    Add code

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-console.log('HEllo');
+//Objects
+
+const newUser = {
+  userName: "Gul",
+  age: 26, 
+  friends: [], //Array
+  hasAccess: false,
+  pet: [
+    {
+      type: 'cat',
+      age: 2
+    },
+    {
+      type: 'dog',
+      age: 3
+    }
+  ], // Array of objects
+  sayhello: function() {
+      console.log('hello');
+  } // function
+};
+
+
+const petProperty = "pet";
+
+console.log(newUser);
+
+//To access an objects's property
+
+//newUser.sayHello(); // Dot notation
+
+console.log(newUser[petProperty]); // Bracket notation
+
+newUser.hasAccess = true;
+
+console.log(newUser.hasAccess); // true
